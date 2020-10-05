27a65ee63870155605061ff1bbd28a86150e38c4
uqi8 <72401462+uqi8@users.noreply.github.com>
2020-10-05 18:38:13 +0300

    some changes

--- Hackerrank_Solutions/arrays.cpp
+++ Hackerrank_Solutions/arrays.cpp
@@ -9,6 +9,8 @@ using namespace std;
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
     int n;
+    int x;
+    int y;
     cin >> n;
 
     int liste[n];
